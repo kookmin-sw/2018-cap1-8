5834373c4f84521f434080a8f2dc64c133e6a359
kjlee <kjlee342@gmail.com>
2018-05-17 16:04:06 +0900

    예측 테스트시 한 파일의 모든 csi를 읽어오도록 수정

--- src/PredictionTest.py
+++ src/PredictionTest.py
@@ -2,23 +2,37 @@ import tensorflow as tf
 import numpy as np
 import csv
 
-xx = np.empty([0,500,90],float)
-f ='test/testempty.csv'
+
+f ='Dataset2/test용/2018_05_14_handup03_01.csv'
 data = [[ float(elm) for elm in v] for v in csv.reader(open(f, "r"))]
 tmp1 = np.array(data)
-xx = np.dstack(tmp1.T)
-x2 = np.empty([0,500,90],float)
-x2 = np.concatenate((x2, xx),axis=0)
-saver = tf.train.import_meta_graph('LR0.0001_BATCHSIZE200_NHIDDEN200/model.ckpt.meta')
+slide_size = 200
+
+saver = tf.train.import_meta_graph('learning2/model.ckpt.meta')
 graph = tf.get_default_graph()
 x = graph.get_tensor_by_name("Placeholder:0")
 pred = graph.get_tensor_by_name("add:0")
-
+arrMatrix = np.zeros((1,5))
 with tf.Session() as sess:
-    saver.restore(sess, 'LR0.0001_BATCHSIZE200_NHIDDEN200/model.ckpt')
+    saver.restore(sess, 'learning2/model.ckpt')
     #sess.run(init)
-    n = pred.eval(feed_dict={x: x2})
-    print(n)
-    n2 = tf.argmax(n, 1)
-    print(n2.eval())
+    k=0
+    count = 0
+    while k <= (len(tmp1) + 1 - 2 * 500):
+        x2 = np.empty([0, 500, 90], float)
+        xx = np.dstack(np.array(tmp1[k:k + 500, 0:90]).T)
+        x2 = np.concatenate((x2, xx), axis=0)
+        k += slide_size
+
+        n = pred.eval(feed_dict={x: x2})
+        print(n)
+        n2 = tf.argmax(n, 1)
+        result = n2.eval()
+        arrMatrix[0][result] += 1
+        print(result)
+        count += 1
+    print()
+    print("walk stand empty sit handup (%)")
+    print((arrMatrix / count) * 100)
+
     sess.close()
